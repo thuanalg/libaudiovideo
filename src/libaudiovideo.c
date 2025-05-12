d2dc8180838bf1a1054bc0ef50cd0d871bb573bb
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-05-12 16:47:34 +0700

    x0

--- src/libaudiovideo.c
+++ src/libaudiovideo.c
@@ -5,12 +5,14 @@
 	spllog(SPL_LOG_BASE, __fmt_____, ##__VA_ARGS__)
 #define lav_dbg(__fmt_____, ...) \
 	spllog(SPL_LOG_DEBUG, __fmt_____, ##__VA_ARGS__)
-#define lav_wrn(__fmt_____, ...) \
+#define lav_inf(__fmt_____, ...) \
 	spllog(SPL_LOG_INFO, __fmt_____, ##__VA_ARGS__)
-#define lav_err(__fmt_____, ...) \
+#define lav_wrn(__fmt_____, ...) \
 	spllog(SPL_LOG_WARNING, __fmt_____, ##__VA_ARGS__)
-#define lav_ftl(__fmt_____, ...) \
+#define lav_err(__fmt_____, ...) \
 	spllog(SPL_LOG_ERROR, __fmt_____, ##__VA_ARGS__)
+#define lav_ftl(__fmt_____, ...) \
+	spllog(SPL_LOG_FATAL, __fmt_____, ##__VA_ARGS__)
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
 int lav_get_dev() {
 	lav_all("Test: %s", __FILE__);
--- tests/console/main.c
+++ tests/console/main.c
@@ -3,10 +3,11 @@ int main(int argc, char *argv[]) {
 	int ret = 0, i = 0;
 	char cfgpath[1024];
 	SPL_INPUT_ARG input = { 0 };
-	snprintf(cfgpath, 1024, "D:/reserach/libaudiovideo/build/Debug/simplelog.cfg");
+	snprintf(cfgpath, 1024, "D:/reserach/libaudiovideo/build/Debug/simplelog.cfgo");
 	snprintf(input.folder, SPL_PATH_FOLDER, "%s", cfgpath);
 	ret = spl_init_log_ext(&input);
 	if (ret) {
+		SPL_LOG_ERR_CODE errcode = ret;
 		const char* p = spl_err_txt(ret);
 		fprintf(stdout, "\n%s\n", p);
 		exit(1);
