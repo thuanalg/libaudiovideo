69c12846a861da70b551623e50938eecacc7908f
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-05-12 15:52:50 +0700

    x0

--- include/libaudiovideo.h
+++ include/libaudiovideo.h
@@ -1,1 +1,29 @@
-/**/
+#ifndef ___LIB_AUDIOVIDEO_H__
+#define ___LIB_AUDIOVIDEO_H__  
+#include "simplelog.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef UNIX_LINUX
+#ifndef __LIB_AUDIOVIDEO_STATIC__
+#ifdef EXPORT_DLL_API_AIDIOVIDEO
+#define DLL_API_AIDIOVIDEO              __declspec(dllexport)
+#else
+#define DLL_API_AIDIOVIDEO              __declspec(dllimport)
+#endif
+#else
+#define DLL_API_AIDIOVIDEO              
+#endif
+#else
+#define DLL_API_AIDIOVIDEO              
+#endif /*! UNIX_LINUX */
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
+DLL_API_AIDIOVIDEO
+int lav_get_dev();
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/libaudiovideo.c
+++ src/libaudiovideo.c
@@ -1,1 +1,19 @@
-/**/
+#include "libaudiovideo.h"
+#include "simplelog.h"
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/	
+#define lav_all(__fmt_____, ...) \
+	spllog(SPL_LOG_BASE, __fmt_____, ##__VA_ARGS__)
+#define lav_dbg(__fmt_____, ...) \
+	spllog(SPL_LOG_DEBUG, __fmt_____, ##__VA_ARGS__)
+#define lav_wrn(__fmt_____, ...) \
+	spllog(SPL_LOG_INFO, __fmt_____, ##__VA_ARGS__)
+#define lav_err(__fmt_____, ...) \
+	spllog(SPL_LOG_WARNING, __fmt_____, ##__VA_ARGS__)
+#define lav_ftl(__fmt_____, ...) \
+	spllog(SPL_LOG_ERROR, __fmt_____, ##__VA_ARGS__)
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
+int lav_get_dev() {
+	lav_all("Test: %s", __FILE__);
+	return 0;
+}
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
